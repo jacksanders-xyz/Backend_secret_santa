edd424b9e3247041f88043fa562ae6b364e025ff
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-17 22:10:38 -0700

    attempting the swap

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,20 @@
 class Group < ApplicationRecord
     has_many :users
+
+
+
+
+    # this will be called to create the list of randomely generated nameDraw 
+    # A new hash, where an Id of a user, will point to the Id of a another user
+
+    def self.draw_and_send
+      # swapped_hash = self.users.each_with_object({}) do |key, hash|
+      # hash[key]
+    end
+
+
+
+
+
+
 end
