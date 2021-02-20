4921c53b29ef8a6e2543f9d80e7d369afcf7301b
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-20 15:02:58 -0700

    better swap

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,6 @@ funGuys = Group.create({
     draw_date: "2020-12-20 11:59:59", 
     game_day: "2020-12-25 11:59:59" 
 })
-notVeryFunGuys = Group.create({ 
-    name: "notVeryFunGuys", 
-    password: "1234234", 
-    draw_date: "2020-23-20 11:59:59", 
-    game_day: "2020-12-25 11:59:59" 
-})
 jack = User.create({
     first_name: "jack",
     last_name: "sanders",
