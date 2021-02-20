e1a8917403424c1b948e09b58c67095e32f0312d
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-20 15:01:42 -0700

    better swap function

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,44 +11,37 @@ class Group < ApplicationRecord
       theUsers = User.select{ |user| user.group_id == self.id}
       theUsers
     end
-
-
-
-  def draw_and_send 
-    containerArray = []
-    theUsers = User.select{ |user| user.group_id == self.id}
-      def shuffler theUsers
-        numberSet = (0..theUsers.length - 1).to_a.shuffle.take(theUsers.length)
-        
-        numberSet.each_with_index do |element, index|
+    
+    def shuffler theUsers
+      numberSet = (0..theUsers.length - 1).to_a.shuffle.take(theUsers.length)
+      numberSet.each_with_index do |element, index|
           if element === index 
             break shuffler (theUsers) 
           else
             nil
           end
-        end
-      numberSet
-      end 
-      numberSet = shuffler theUsers
+      end
+    end 
+
+    def draw_and_send 
+      containerArray = []
+      theUsers = User.select{ |user| user.group_id == self.id}
+      numberSet = self.shuffler theUsers
       theUsers = theUsers.each_with_index do |element, index| 
-            containerArray << element = {
-              id: element[:id],
-              first_name: element[:first_name],
-              last_name: element[:last_name],
-              email_address: element[:email_address],
-              group_id: element[:group_id],
-              interests: element[:interests],
-              created_at: element[:created_at],
-              updated_at: element[:updated_at],
-              their_pick: theUsers[numberSet[index]]
-            } 
-      end 
-      pp containerArray
-  end
-    
-
-
-
+              containerArray << element = {
+                id: element[:id],
+                first_name: element[:first_name],
+                last_name: element[:last_name],
+                email_address: element[:email_address],
+                group_id: element[:group_id],
+                interests: element[:interests],
+                created_at: element[:created_at],
+                updated_at: element[:updated_at],
+                their_pick: theUsers[numberSet[index]]
+              } 
+        end 
+      containerArray
+    end
 end
 
 
